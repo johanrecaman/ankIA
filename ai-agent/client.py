6263c7ac8d126bb735876d7fd70ae3d5329604ff
Johan Recaman <johanstrr@gmail.com>
2025-05-30 15:56:18 -0300

    python receiving pdf

--- ai-agent/client.py
+++ ai-agent/client.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 from dotenv import load_dotenv
-from typing import Annotated, List
+from typing import Annotated, List, BinaryIO
 from typing_extensions import TypedDict
+
+from io import BytesIO
 
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.tools import tool
@@ -14,10 +16,24 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.prebuilt import create_react_agent
+
+import PyPDF2
+
 from datetime import datetime
 import requests
 
 load_dotenv()
+
+def pdf_extract(pdf: BinaryIO) -> str:
+    complete_text = ""
+
+    reader = PyPDF2.PdfReader(pdf)
+    for page in reader.pages:
+        text = page.extract_text()
+        if text:
+            complete_text += text + "\n"
+    return complete_text
+
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
@@ -48,4 +64,24 @@ workflow.add_edge(START, "analyzer_agent")
 workflow.add_edge("analyzer_agent", "card_generator_agent")
 
 graph = workflow.compile()
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/upload-pdf/")
+async def upload_pdf(file: UploadFile = File(...)):
+    content = await file.read()
+    buffer = BytesIO(content)
+
+    text = pdf_extract(buffer)
+    return text
+
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import React, {useState} from "react";
+
 function App() {
+
+  const [file, setFile] = useState<File>();
+
+  const handleSubmit = async () =>{
+    const formData = new FormData();
+    formData.append("file", file)
+    const response = await fetch("http://localhost:5050/upload-pdf",{
+      method: "POST",
+      body: formData
+  });
+
+  const data = await response.json();
+  console.log(data)
+  }
 
   return (
     <>
+      <input type="file" onChange={(e)=> setFile((e.target as HTMLInputElement).files[0])}/>
+      <button onClick={handleSubmit}>submit</button>
     </>
   )
 }
